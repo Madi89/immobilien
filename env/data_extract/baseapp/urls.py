405c22310f9da22ee4eef67a9d0b5619c7245789
Madi <madeline-hessel@gmx.de>
2023-12-11 11:18:57 +0100

    objektdetails

--- env/data_extract/baseapp/urls.py
+++ env/data_extract/baseapp/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
 
     path('test/', TestView.as_view(), name='test'),
     path('objekt_liste/', TerminObjektlisteView.as_view(), name='objekt_liste'),
-    path('test_objektdetails/<int:url_zvg_index>/', TestObjektdetails.as_view() , name='test_objektdetails')
+    path('test_objektdetails/', ObjektdetailsSucheView.as_view() , name='test_objektdetails')
 ]
